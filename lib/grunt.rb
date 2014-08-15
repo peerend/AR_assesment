2f7e1e6d0255380eeeb4709e6fce3ca73267836e
Dylan Peerenboom <peerend@gmail.com>
2014-08-15 14:58:59 -0700

    capitalize names with a callback

--- lib/grunt.rb
+++ lib/grunt.rb
@@ -1,8 +1,15 @@
 class Grunt < ActiveRecord::Base
 
+  before_save :capitalize_name
+
   belongs_to :ships
 
   validates :name, presence: true
   validates :ships_id, presence: true
 
+private
+
+  def capitalize_name
+    self.name = self.name.capitalize
+  end
 end
--- spec/grunt_spec.rb
+++ spec/grunt_spec.rb
@@ -5,8 +5,14 @@ describe Grunt do
     grunt = Grunt.create({:name => '', :ships_id =>1})
     expect(Grunt.all).to eq []
   end
+
   it 'validates a ship_id' do
     grunt = Grunt.create({:name => 'Picard'})
     expect(Grunt.all).to eq []
+  end
+
+  it 'capitalizes the name' do
+    grunt = Grunt.create(:name => "piCaRd", :ships_id =>1)
+    grunt.name.should eq "Picard"
   end
 end
